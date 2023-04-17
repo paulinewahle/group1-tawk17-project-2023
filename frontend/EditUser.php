7ab647d4591b235d1f274382fb705f4d580a3864
Anne Thieß <anne.thiess12@gmail.com>
2023-04-17 10:03:12 +0200

    Edit Profile HTML Form

--- frontend/EditUser.php
+++ frontend/EditUser.php
@@ -12,3 +12,25 @@ class UserModel{
     public $first_name;
     public $last_name;
 }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Edit Profile</title>
+  </head>
+  <body>
+    <h1>Edit Profile</h1>
+    <form action="process-edit-profile.php" method="POST">
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" required><br><br>
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required><br><br>
+      <label for="location">Location:</label>
+      <input type="text" id="location" name="location"><br><br>
+      <label for="premium">Premium:</label>
+      <input type="checkbox" id="premium" name="premium"><br><br>
+      <input type="submit" value="Save Changes">
+    </form>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,20 +1,20 @@
-<?php
+<!-- <?php
 //Implement API  OpenWeatherMap
-if (isset($_POST["submit"])) {
-    if (empty($_POST["city"])) {
-        echo "Enter your City";
-    }else{
-        $city = $_POST["city"];
-        $api_key = "a74c60a34f798eefa00278309f5c1b24";
-        $api = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key";
-        $api_data = file_get_contents($api);
-        //print_r($api_data);
+// if (isset($_POST["submit"])) {
+//     if (empty($_POST["city"])) {
+//         echo "Enter your City";
+//     }else{
+//         $city = $_POST["city"];
+//         $api_key = "a74c60a34f798eefa00278309f5c1b24";
+//         $api = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key";
+//         $api_data = file_get_contents($api);
+//         //print_r($api_data);
 
-        $weather = json_decode($api_data, true);
-        $celcius = $weather["main"]["temp"] - 273;
-        //print_r($weather);
-    }
-}
+//         $weather = json_decode($api_data, true);
+//         $celcius = $weather["main"]["temp"] - 273;
+//         //print_r($weather);
+//     }
+// }
 
 ?>
 
@@ -55,4 +55,4 @@ if (isset($_POST["submit"])) {
        
     </div>
 </body>
-</html>
+</html> -->
