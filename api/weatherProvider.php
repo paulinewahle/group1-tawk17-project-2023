17840b3a5b22b61e39208f505767f2b7d8bdf099
Anne Thieß <anne.thiess12@gmail.com>
2023-04-17 09:34:37 +0200

    kfndlkfs

--- api/weatherProvider.php
+++ api/weatherProvider.php
@@ -29,3 +29,23 @@
      $apiKey="old key"
   }
  ?>
+ */
+
+ <?php
+//Implement API  OpenWeatherMap
+if (isset($_POST["submit"])) {
+    if (empty($_POST["city"])) {
+        echo "Enter your City";
+    }else{
+        $city = $_POST["city"];
+        $api_key = "a74c60a34f798eefa00278309f5c1b24";
+        $api = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key";
+        $api_data = file_get_contents($api);
+        //print_r($api_data);
+
+        $weather = json_decode($api_data, true);
+        $celcius = $weather["main"]["temp"] - 273;
+        //print_r($weather);
+    }
+}
+?>
