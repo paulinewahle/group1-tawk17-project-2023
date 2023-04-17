3ccd72c1a2788873d2a30fa176a3caf8b42dae91
Anne Thieß <anne.thiess12@gmail.com>
2023-04-17 11:07:47 +0200

    apis

--- api/WeatherAPI.php
+++ api/WeatherAPI.php
@@ -24,7 +24,7 @@ if ($user->isPremium()) {
         }else{
             $city = $_POST["city"];
             $api_key = "a74c60a34f798eefa00278309f5c1b24";
-            $api = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key";
+            $api = "api.openweathermap.org/data/2.5/forecast?weather?q=$city&appid=$api_key";
             $api_data = file_get_contents($api);
             $weather = json_decode($api_data, true);
             $celcius = $weather["main"]["temp"] - 273;
