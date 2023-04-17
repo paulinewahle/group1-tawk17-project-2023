08672b44746760d4421bcd946c6ec46df21bfda5
Pauline Wahle <pauline.wahle@web.de>
2023-04-17 11:09:10 +0200

    Merge branch 'main' of https://github.com/paulinewahle/group1-tawk17-project-2023

--- api/WeatherAPI.php
+++ api/WeatherAPI.php
@@ -16,21 +16,21 @@
 //       $user->updateProfile($username, $password, $location, $is_premium);
 //     }
 
-//     // Normal User Get Weather Data
-//    else {
-//     if (isset($_POST["submit"])) {
-//         if (empty($_POST["city"])) {
-//             echo "Enter your City";
-//         }else{
-//             $city = $_POST["city"];
-//             $api_key = "a74c60a34f798eefa00278309f5c1b24";
-//             $api = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key";
-//             $api_data = file_get_contents($api);
-//             $weather = json_decode($api_data, true);
-//             $celcius = $weather["main"]["temp"] - 273;
-//         }
-//     }
-//   }
+    // Normal User Get Weather Data
+   else {
+    if (isset($_POST["submit"])) {
+        if (empty($_POST["city"])) {
+            echo "Enter your City";
+        }else{
+            $city = $_POST["city"];
+            $api_key = "a74c60a34f798eefa00278309f5c1b24";
+            $api = "api.openweathermap.org/data/2.5/forecast?weather?q=$city&appid=$api_key";
+            $api_data = file_get_contents($api);
+            $weather = json_decode($api_data, true);
+            $celcius = $weather["main"]["temp"] - 273;
+        }
+    }
+  }
 
 
 ?>
