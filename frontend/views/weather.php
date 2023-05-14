0f51b49345d8cff973e50f8e74486b7f04807390
Anne Thieß <anne.thiess12@gmail.com>
2023-05-14 13:11:04 +0200

    a

--- frontend/views/weather.php
+++ frontend/views/weather.php
@@ -1,36 +1,47 @@
 <?php
 require_once __DIR__ . "/../Template.php";
 
-Template::header("Articles"); 
+Template::header("Weather"); 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Weather</title>
+</head>
 
-<p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Tempora nam vitae quos nisi, eius delectus libero quia at, 
-    veritatis ipsa facere obcaecati nihil possimus non, dolorem 
-    molestias temporibus laboriosam totam.
-</p>
+<body>
+    <section>
+    <form method="post">
+    <h1>Weather App</h1>
+    <button class="edit-user-btn">Edit User</button>
+    <input type = "text" name = "city" id = "">
+    <input name = "submit" type = "submit" value = "Check Weather">
 
-<p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Tempora nam vitae quos nisi, eius delectus libero quia at, 
-    veritatis ipsa facere obcaecati nihil possimus non, dolorem 
-    molestias temporibus laboriosam totam.
-</p>
 
-<p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Tempora nam vitae quos nisi, eius delectus libero quia at, 
-    veritatis ipsa facere obcaecati nihil possimus non, dolorem 
-    molestias temporibus laboriosam totam.
-</p>
-
-<p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Tempora nam vitae quos nisi, eius delectus libero quia at, 
-    veritatis ipsa facere obcaecati nihil possimus non, dolorem 
-    molestias temporibus laboriosam totam.
-</p>
+    <div class="weather-container">
+        
+        <div class="location">
+        <h2> <?= $city ?></h2>
+        </div>
+        <br>
+        <div class="weatherdescription">
+        <h2> <?= $weather["weather"][0]["description"] ?></h2>
+        </div>
+        <br>
+        <div class="temperature">
+        <h2> <?= $celcius?> °C </h2>
+        </div>
+        <div class="weather-icon">
+            <img src="weatherIcons/day/176.png" alt="Weather Icon">
+        </div>
+       
+    </div>
+</body>
+</html>
 
 <?php Template::footer(); ?>
