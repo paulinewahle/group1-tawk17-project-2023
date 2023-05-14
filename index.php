685189f3e1b50d1edd19de3be205faacd77288eb
Anne Thieß <anne.thiess12@gmail.com>
2023-05-14 11:25:16 +0200

    Push

--- index.php
+++ index.php
@@ -5,30 +5,33 @@ define('MY_APP', true);
 
 // Load the router responsible for handling API requests
 require_once __DIR__ . "/api/APIRouter.php";
-require_once __DIR__."/frontend/WeatherPage.php";
+require_once __DIR__."/frontend/FrontendRouter.php";
 
 // Get URL path
 $path = $_GET["path"];
 $path_parts = explode("/", $path);
 $base_path = strtolower($path_parts[0]);
 
-// var_dump($path_parts, $base_path);
-// die();
 
 // If the URL path starts with "api", load the API
 if($base_path == "api" && count($path_parts) > 1){
     $query_params = $_GET;
-    // Handle requests using the API router
-    $api = new APIRouter($path_parts, $query_params);
-    $api->handleRequest();
+    
+ // Handle requests using the API router
+ $api = new FrontendRouter($path_parts, $query_params);
+ $api->handleRequest();
+
 }
-else if($base_path == "weather" && count($path_parts) > 1){
-    //! Fronend router
+
+else if($base_path == "home" && count($path_parts) > 1){
+    $query_params = $_GET;
+
+        //Handle request using API router
+        $frontend = new APIRouter($path_parts, $query_params);
+        $frontend -> handleRequest();
 }
 else{ // If URL path is not API, respond with "not found"
-    http_response_code(404);
+    //http_response_code(404);
+    header('Location: home');
     die("Page not found");
 }
-
-?>
-
