9de8a42226e0801d4640e389783e957fd1e3a314
Anne Thieß <anne.thiess12@gmail.com>
2023-05-19 13:13:03 +0200

    Weather api

--- data-access/weatherdataaccess.php
+++ data-access/weatherdataaccess.php
@@ -1,78 +1,41 @@
 <?php
 
-require_once __DIR__ . "/RestAPI.php";
-require_once __DIR__ . "/UsersAPI.php";
-require_once __DIR__ . "/APIRouter.php";
-require_once __DIR__ . "/../business-logic/UsersService.php";
-
-
+// Check for a defined constant or specific file inclusion
 if (!defined('MY_APP') && basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
     die('This file cannot be accessed directly.');
 }
 
+$apiKey = 'a74c60a34f798eefa00278309f5c1b24Y';
+$city = 'London'; 
 
-// Class for handling requests to "api/User"
+// API URL
+$url = "https://api.openweathermap.org/data/2.5/weather?q=" . urlencode($city) . "&appid=" . $apiKey;
 
-class WeatherAPI extends RestAPI
-{
-    // Handles the request by calling the appropriate member function
-    public function handleRequest()
-    {
-        if ($this->path_count == 3 && $this->method == "GET") {
-            $this->checkById($this->path_parts[2]);
-        }
-        else {
-            $this->notFound();
-        }
-    } 
+// Make API request and get the response
+$response = file_get_contents($url);
 
-    if (isset($_POST["submit"])) {
-                   
-        echo "Enter your City";
-    
-        $city = $_POST["city"];
-        $api_key = "a74c60a34f798eefa00278309f5c1b24";
-        $api = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key";
-        $api_data = file_get_contents($api);
-        $weather = json_decode($api_data, true);
-        $celcius = $weather["main"]["temp"] - 273;
-                    
-    }
-    // private function checkById($id) {
-    //     $user = UsersService::getUserById($id); 
-    //     if ($user->isPremium) { 
-    //         if (isset($_POST["submit"])) {
-    //             if (empty($_POST["city"])) {
-    //                 echo "Enter your City";
-    //             }else{
-    //                 $city = $_POST["city"];
-    //                 $api_key = "a74c60a34f798eefa00278309f5c1b24";
-    //                 $api = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key";
-    //                 $api_data = file_get_contents($api);
-    //                 $weather = json_decode($api_data, true);
-    //                 $celcius = $weather["main"]["temp"] - 273;
-    //             }
-    //         }
-          
-    //     }
-        
-    //     // Normal User Get Weather Data
-    //     else {
-    //         if (isset($_POST["submit"])) {
-    //             if (empty($_POST["city"])) {
-    //                 echo "Enter your City";
-    //             }else{
-    //                 $city = $_POST["city"];
-    //                 $api_key = "a74c60a34f798eefa00278309f5c1b24";
-    //                 $api = "api.openweathermap.org/data/2.5/forecast?weather?q=$city&appid=$api_key";
-    //                 $api_data = file_get_contents($api);
-    //                 $weather = json_decode($api_data, true);
-    //                 $celcius = $weather["main"]["temp"] - 273;
-    //             }
-    //         }
-    //     }
-    }
+// Convert JSON response to PHP array
+$data = json_decode($response, true);
 
+// Check if the API request was successful
+if ($data['cod'] === 200) {
+    // Weather data is available
+    $temperature = $data['main']['temp'];
+    $description = $data['weather'][0]['description'];
+    $icon = $data['weather'][0]['icon'];
+} else {
+    // API request failed, handle the error
+    $errorMessage = $data['message'];
+    // Handle the error gracefully (e.g., display a default value or an error message)
 }
 
-?> 
+
+class WeatherDataAccess{
+        $city = $_POST["city"];
+                    $api_key = "a74c60a34f798eefa00278309f5c1b24";
+                    $api = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key";
+                    $api_data = file_get_contents($api);
+                    $weather = json_decode($api_data, true);
+                    $celcius = $weather["main"]["temp"] - 273;
+}
+?>
